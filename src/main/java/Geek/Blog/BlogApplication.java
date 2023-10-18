c0fdcd5eb44548ff56fb6f3709b09cfbd3d9c209
ms987796 <lsh929500@gmail.com>
2023-10-18 12:00:49 +0900

    Change user test2

--- src/main/java/Geek/Blog/BlogApplication.java
+++ src/main/java/Geek/Blog/BlogApplication.java
@@ -8,7 +8,7 @@ public class BlogApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
-		System.out.println("Hi");
+		System.out.println("Hi this is second try");
 	}
 
 }
