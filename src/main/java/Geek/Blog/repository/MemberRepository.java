3669353ab8983236efbbc6520acbc4400f6b4c10
ms987796 <ms987796@naver.com>
2023-10-21 00:37:27 +0900

    member table 추가 및 로그인 구현

---
+++ src/main/java/Geek/Blog/repository/MemberRepository.java
@@ -0,0 +1,12 @@
+package Geek.Blog.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import Geek.Blog.entity.Member;
+
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
+}
