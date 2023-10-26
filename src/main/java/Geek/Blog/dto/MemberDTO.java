9731fc24b7f07f7c78c684ab22648e26eda22652
ms987796 <ms987796@naver.com>
2023-10-26 09:40:04 +0900

    member table 수정

--- src/main/java/Geek/Blog/dto/MemberDTO.java
+++ src/main/java/Geek/Blog/dto/MemberDTO.java
@@ -15,6 +15,7 @@ public class MemberDTO { //회원 정보를 필드로 정의
     private String name;
     private String email;
     private String password;
+    private String interest;
 
     //lombok 어노테이션으로 getter,setter,생성자,toString 메서드 생략 가능
     public static MemberDTO toMemberDTO(Member memberEntity){
@@ -23,6 +24,7 @@ public class MemberDTO { //회원 정보를 필드로 정의
         memberDTO.setEmail(memberEntity.getEmail());
         memberDTO.setName(memberEntity.getName());
         memberDTO.setPassword(memberEntity.getPassword());
+        memberDTO.setInterest(memberEntity.getInterest());
 
         return memberDTO;
     }
