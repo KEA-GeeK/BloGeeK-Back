613f32eb79c8ebc99dbcab86c22e2bdb394a82ff
ms987796 <ms987796@naver.com>
2023-10-26 11:10:45 +0900

    팔로우 엔티티 생성

---
+++ src/main/java/Geek/Blog/entity/Follow.java
@@ -0,0 +1,31 @@
+package Geek.Blog.entity;
+
+import Geek.Blog.dto.FollowDTO;
+import Geek.Blog.dto.MemberDTO;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "Follow")
+public class Follow {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto increment
+    private Long id;
+
+    @Column
+    private Long follower_id;
+
+    @Column
+    private Long following_id;
+
+    public static Follow toFollowEntity(FollowDTO followDTO){
+        Follow followEntity = new Follow();
+        followEntity.setId(followDTO.getId());
+        followEntity.setFollower_id(followDTO.getFollower_id());
+        followEntity.setFollowing_id(followDTO.getFollowing_id());
+        return followEntity;
+    }
+}
---
+++ src/main/java/Geek/Blog/repository/FollowRepository.java
@@ -0,0 +1,11 @@
+package Geek.Blog.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FollowRepository {
+
+    //디비에서 검색
+    //-->서비스로 넘기기
+    //findBy
+}
