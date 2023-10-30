c5c4530c7cf43ab08357c7eed7d43fe0c43aaad9
ms987796 <ms987796@naver.com>
2023-10-31 03:25:19 +0900

    회원가입/회원조회/회원탈퇴 api구현

--- src/main/java/Geek/Blog/controller/MemberController.java
+++ src/main/java/Geek/Blog/controller/MemberController.java
@@ -1,10 +1,13 @@
 package Geek.Blog.controller;
 
+import Geek.Blog.dto.MemberDTO;
 import Geek.Blog.entity.Member;
 import Geek.Blog.repository.MemberRepository;
 import Geek.Blog.service.MemberService;
+import ch.qos.logback.core.model.Model;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -16,6 +19,10 @@ public class MemberController {
     private final MemberService memberService;
     private final MemberRepository memberRepository;
 
+    @GetMapping("/save")
+    public String saveForm(){
+        return "save~";
+    }
     @PostMapping("/save")    //name값을 repquestparam에 담아온다.
     public Member save(@RequestBody Member member) {
         log.info(member.toString());
@@ -31,6 +38,27 @@ public class MemberController {
         } catch (Exception e) {
             log.error(e.getMessage());
             return "fail";
+        }
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MemberDTO> findBy(@PathVariable Long id) {
+        MemberDTO memberDTO = memberService.findById(id);
+        if (memberDTO != null) {
+            return ResponseEntity.ok(memberDTO);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/delete/{id}")
+    public ResponseEntity<MemberDTO> deleteById(@PathVariable Long id) {
+        MemberDTO deletedMember = memberService.deleteById(id);
+        if (deletedMember != null) {
+            return ResponseEntity.ok(deletedMember);
+        } else {
+            return ResponseEntity.notFound().build();
         }
     }
 }
--- src/main/java/Geek/Blog/service/MemberService.java
+++ src/main/java/Geek/Blog/service/MemberService.java
@@ -1,9 +1,12 @@
 package Geek.Blog.service;
 
+import Geek.Blog.dto.MemberDTO;
 import Geek.Blog.entity.Member;
 import Geek.Blog.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 
 @Service
@@ -12,12 +15,35 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    public void save(MemberDTO memberDTO){
+        Member member = Member.toMemberEntity(memberDTO);
+        memberRepository.save(member);
+    }
+
+
     // Todo: Member Class 대신 LoginReqDto 받아서 id를 기반으로 사용자 조회하고 로그인 처리하도록 수정
     public Member login(Member member) throws Exception {
         return memberRepository.findByEmail(member.getEmail())
                 .orElseThrow(() -> new Exception("존재하지 않는 사용자"));
     }
 
+    public MemberDTO findById(Long id) {
+        Optional<Member> optionalMemberEntity = memberRepository.findById(id);
+        if (optionalMemberEntity.isPresent()){
+            return MemberDTO.toMemberDTO(optionalMemberEntity.get()); // optional을 벗겨내서 entity -> dto 변환
+        }else {
+            return null;
+        }
+    }
 
+    public MemberDTO deleteById(Long id) {
+        Optional<Member> optionalMemberEntity = memberRepository.findById(id);
+        if (optionalMemberEntity.isPresent()) {
+            Member member = optionalMemberEntity.get();
+            memberRepository.deleteById(id);
+            return MemberDTO.toMemberDTO(member);
+        } else {
+            return null;
+        }
+    }
 }
-
