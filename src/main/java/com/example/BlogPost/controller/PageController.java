632022d24732ac1e68cd763c23a2a27f0f817f53
xornjswldh <86345585+xornjswldh@users.noreply.github.com>
2023-11-11 00:01:10 +0900

    Post, Comment, Reply(미검증)

--- src/main/java/com/example/BlogPost/controller/PageController.java
+++ src/main/java/com/example/BlogPost/controller/PageController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-//TODO Comment Controller, Reply들 추가
+//TODO Comment / Reply 검사
 @Controller
 public class PageController {
 
